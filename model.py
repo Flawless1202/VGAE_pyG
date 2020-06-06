b1e752b49ec17a516e72f54b72e44f53b4e6da80
Chen Kai <1911043@tongji.edu.cn>
2020-06-06 10:51:06 +0800

    bug fixed and achieve the ROC_AUC Score and AP Score matching the author of pytorch geometric

--- model.py
+++ model.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 
 from torch_geometric.nn.models import InnerProductDecoder, VGAE
 from torch_geometric.nn.conv import GCNConv
-from torch_geometric.utils import negative_sampling
+from torch_geometric.utils import negative_sampling, remove_self_loops, add_self_loops
 
 
 class GCNEncoder(nn.Module):
@@ -33,15 +33,25 @@ class DeepVGAE(VGAE):
         adj_pred = self.decoder.forward_all(z)
         return adj_pred
 
-    def loss(self, x, pos_edge_index):
+    def loss(self, x, pos_edge_index, all_edge_index):
         z = self.encode(x, pos_edge_index)
-        recon_loss = self.recon_loss(z, pos_edge_index)
-        kl_loss = self.kl_loss()
-        return recon_loss + kl_loss
 
-    def single_test(self, x, pos_edge_index):
-        neg_edge_index = negative_sampling(pos_edge_index, x.size(0))
-        z = self.encode(x, pos_edge_index)
-        pos_edge_index_tmp = pos_edge_index
-        roc_auc_score, average_precision_score = self.test(z, pos_edge_index_tmp, neg_edge_index)
+        pos_loss = -torch.log(
+            self.decoder(z, pos_edge_index, sigmoid=True) + 1e-15).mean()
+
+        # Do not include self-loops in negative samples
+        all_edge_index_tmp, _ = remove_self_loops(all_edge_index)
+        all_edge_index_tmp, _ = add_self_loops(all_edge_index_tmp)
+
+        neg_edge_index = negative_sampling(all_edge_index_tmp, z.size(0), pos_edge_index.size(1))
+        neg_loss = -torch.log(1 - self.decoder(z, neg_edge_index, sigmoid=True) + 1e-15).mean()
+
+        kl_loss = 1 / x.size(0) * self.kl_loss()
+
+        return pos_loss + neg_loss + kl_loss
+
+    def single_test(self, x, train_pos_edge_index, test_pos_edge_index, test_neg_edge_index):
+        with torch.no_grad():
+            z = self.encode(x, train_pos_edge_index)
+        roc_auc_score, average_precision_score = self.test(z, test_pos_edge_index, test_neg_edge_index)
         return roc_auc_score, average_precision_score
--- train.py
+++ train.py
@@ -3,39 +3,35 @@ import torch.nn as nn
 from torch.optim import Adam
 
 from torch_geometric.datasets import Planetoid
-from torch_geometric.utils import to_dense_adj, negative_sampling
+import torch_geometric.transforms as T
+from torch_geometric.utils import train_test_split_edges
 
 from model import DeepVGAE
 from config.config import parse_args
 
+torch.manual_seed(12345)
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
 args = parse_args()
 
-dataset = Planetoid("datasets", "citeSeer")
-
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model = DeepVGAE(args).to(device)
+optimizer = Adam(model.parameters(), lr=args.lr)
+
+dataset = Planetoid("datasets", "Cora", transform=T.NormalizeFeatures())
 data = dataset[0].to(device)
-optimizer = Adam(model.parameters(), lr=args.lr, weight_decay=5e-4)
-
-# pos_edge_index = torch.tensor([[0, 1, 1, 2],
-#                            [1, 0, 2, 1]],
-#                           dtype=torch.long)
-# dense_adj = to_dense_adj(pos_edge_index)
-# neg_edge_index = negative_sampling(pos_edge_index, 3)
-# print(neg_edge_index)
-
-# print(data.edge_index.size())
-# print(data.x.size())
-
-neg_samples = negative_sampling(data.edge_index, data.x.size(0))
+all_edge_index = data.edge_index
+data = train_test_split_edges(data, 0.05, 0.1)
 
 for epoch in range(args.epoch):
     model.train()
     optimizer.zero_grad()
-    loss = model.loss(data.x, data.edge_index)
+    loss = model.loss(data.x, data.train_pos_edge_index, all_edge_index)
     loss.backward()
     optimizer.step()
-    if epoch % 5 == 0:
+    if epoch % 2 == 0:
         model.eval()
-        roc_auc_score, average_precision_score = model.single_test(data.x, data.edge_index)
-        print("Epoch {} - Loss: {} ROC_AUC: {} Precision: {}".format(epoch, loss.cpu().item(), roc_auc_score, average_precision_score))
+        roc_auc, ap = model.single_test(data.x,
+                                        data.train_pos_edge_index,
+                                        data.test_pos_edge_index,
+                                        data.test_neg_edge_index)
+        print("Epoch {} - Loss: {} ROC_AUC: {} Precision: {}".format(epoch, loss.cpu().item(), roc_auc, ap))
